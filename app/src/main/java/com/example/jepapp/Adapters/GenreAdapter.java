551946f0bf8e876e8932220011993a5cf23f62ac
pixie21 <49859749+pixie21@users.noreply.github.com>
2019-07-12 03:42:08 +0800

    MenuType Adapter added

---
+++ app/src/main/java/com/example/jepapp/Adapters/GenreAdapter.java
@@ -0,0 +1,95 @@
+package com.example.jepapp.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.jepapp.BreakfastList;
+import com.example.jepapp.Fragments.MakeanOrder;
+import com.example.jepapp.Models.Genre;
+import com.example.jepapp.R;
+import com.example.jepapp.Fragments.SnackListing;
+import com.example.jepapp.SpecialRequests;
+
+import java.util.List;
+
+
+public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Genre> musicGenres;
+    private Genre bg;
+    private String uname;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.title);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        }
+    }
+
+
+    public GenreAdapter(Context mContext, List<Genre> musicGenres, String Username) {
+        this.mContext = mContext;
+        this.musicGenres = musicGenres;
+        this.uname=Username;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.menutype_row, parent, false);
+        final TextView genreView = itemView.findViewById(R.id.title);
+        final ImageView musicCover = itemView.findViewById(R.id.thumbnail);
+        musicCover.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View itemView){
+
+                String genre=genreView.getText().toString();
+
+                if (genre == "Breakfast"){
+                    Intent intent=new Intent(itemView.getContext(), BreakfastList.class);
+                    itemView.getContext().startActivity(intent);
+                }
+                if (genre == "Lunch"){
+                    Intent intent=new Intent(itemView.getContext(), SpecialRequests.class);
+                    itemView.getContext().startActivity(intent);
+                }
+
+            }
+        });
+
+        return new MyViewHolder(itemView);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Genre genre = musicGenres.get(position);
+        final String tit=genre.getName();
+        holder.title.setText(genre.getName());
+
+        Glide.with(mContext).load(genre.getThumbnail()).into(holder.thumbnail);
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return musicGenres.size();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/jepapp/MenuType.java
@@ -0,0 +1,132 @@
+package com.example.jepapp;
+
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.jepapp.Adapters.GenreAdapter;
+import com.example.jepapp.Models.Genre;
+
+public class MenuType extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private GenreAdapter adapter;
+    private List<Genre> musicGenres;
+
+
+    private String usename;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menutype);
+
+
+
+        usename=getIntent().getStringExtra("UN");
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        musicGenres = new ArrayList<>();
+        adapter = new GenreAdapter(this, musicGenres,usename);
+
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
+        displayGenres();
+
+    }
+
+
+    public void setUsename(String usename) {
+        this.usename = usename;
+    }
+
+    public String getUsename() {
+        return usename;
+    }
+
+    private void displayGenres() {
+        int[] covers = new int[]{
+                R.drawable.breakfast,
+                R.drawable.lunch,
+
+        };
+
+        Genre a = new Genre("Breakfast", covers[0]);
+        musicGenres.add(a);
+
+        a = new Genre("Lunch", covers[1]);
+        musicGenres.add(a);
+
+
+
+
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+
+}
